205b9e7a601c549459f29b7fe7dd6d2dc99e4f23
Shapovalov Dmitry <mr.demon4ik@gmail.com>
2024-10-09 12:59:29 +0300

    устранение eager

--- src/main/java/ru/itmentor/spring/boot_security/demo/repositories/UserRepository.java
+++ src/main/java/ru/itmentor/spring/boot_security/demo/repositories/UserRepository.java
@@ -1,7 +1,8 @@
 package ru.itmentor.spring.boot_security.demo.repositories;
 
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.itmentor.spring.boot_security.demo.model.User;
 
@@ -9,6 +10,6 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
-    @EntityGraph(attributePaths = {"roles"})
-    Optional<User> findByUsername(String  username);
+       @Query("SELECT u FROM User u JOIN FETCH u.roles WHERE u.username = :username")
+       Optional<User> findByUsername( @Param("username") String  username);
 }
